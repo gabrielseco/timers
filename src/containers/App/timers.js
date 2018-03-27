0221dd8e2dcb317326ee45bf6239d753ad86e865
Gabriel <ggarciaseco@gmail.com>
2018-03-27 13:07:41 +0200

    feat: When a date is change, change the month too

--- src/containers/App/timers.js
+++ src/containers/App/timers.js
@@ -1,11 +1,21 @@
+import LuxonDate from './../../utils/luxon-date';
+
+const getMonth = date => {
+  const luxon = new LuxonDate(date);
+  const month = luxon.getMonth();
+  return month.slice(0, 1).toUpperCase() + month.slice(1);
+};
+
+const datePayment = new Date(2018, 3, 27, 10, 0, 0);
+
 const timers = [
   {
     title: 'Paga Extra',
     date: new Date(2018, 5, 28, 10, 0, 0)
   },
   {
-    title: 'Nómina Marzo',
-    date: new Date(2018, 3, 27, 10, 0, 0)
+    title: `Nómina ${getMonth(datePayment)}`,
+    date: datePayment
   }
 ];
 
