fbfa2e73ed15b53ba76a3226cbc3259ba1cfd247
Gabriel <ggarciaseco@gmail.com>
2018-03-28 11:03:31 +0200

    chore: Change second Countdown

--- src/containers/App/timers.js
+++ src/containers/App/timers.js
@@ -1,12 +1,17 @@
-import LuxonDate from './../../utils/luxon-date';
-
-const getMonth = date => {
+/* import LuxonDate from './../../utils/luxon-date';
+ */
+/* const getMonth = date => {
   const luxon = new LuxonDate(date);
   const month = luxon.getMonth();
   return month.slice(0, 1).toUpperCase() + month.slice(1);
-};
+}; */
 
-const datePayment = new Date(2018, 3, 27, 10, 0, 0);
+/* const datePayment = new Date(2018, 3, 27, 10, 0, 0); */
+
+/* const normalPayment = {
+  title: `Nómina ${getMonth(datePayment)}`,
+  date: datePayment
+}; */
 
 const timers = [
   {
@@ -14,8 +19,8 @@ const timers = [
     date: new Date(2018, 5, 28, 10, 0, 0)
   },
   {
-    title: `Nómina ${getMonth(datePayment)}`,
-    date: datePayment
+    title: `Desayuno Iñaki`,
+    date: new Date(2018, 3, 13, 10, 0, 0)
   }
 ];
 
